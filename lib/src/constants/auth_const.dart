b1bebab500773a4854133f8ff9b125147adb903d
GermanPrudivus <german.prudivus1008@gmail.com>
2023-07-17 01:10:49 +0300

    modified:   lib/src/constants/auth_const.dart

--- lib/src/constants/auth_const.dart
+++ lib/src/constants/auth_const.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
-String loginSvg = "https://mznfnrsqtgzhhltmpwwh.supabase.co/storage/v1/object/sign/rv1g_info/assets/undraw_login_re_4vu2.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJydjFnX2luZm8vYXNzZXRzL3VuZHJhd19sb2dpbl9yZV80dnUyLnN2ZyIsImlhdCI6MTY4ODU4MTg2NiwiZXhwIjoyMzE5MzAxODY2fQ.mu2O5uYYDMLYrtLSzlWv3gJlqzF2rZMDp0WXIO6TvsM&t=2023-07-05T18%3A31%3A06.862Z";
+String loginSvg = "https://mznfnrsqtgzhhltmpwwh.supabase.co/storage/v1/object/public/assets/undraw_Login_re_4vu2%201.svg";
 Color blue = const Color.fromRGBO(43, 86, 147, 1);
 Color lightGrey = const Color.fromRGBO(172, 177, 193, 1);
 Color transparentLightGrey = const Color.fromRGBO(172, 177, 193, 0.70);
